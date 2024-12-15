b11ece62b2bea71819377335c061afc5331c9829
Leonardo <lbruni10@gmail.com>
2024-12-15 00:27:28 -0300

    commit

---
+++ AutoPartsApp/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using AutoPartsApp.Context;
+using AutoPartsApp.Models.User;
+using AutoPartsApp.Models.User.Request;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AutoPartsApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AutoPartsAppContext _dbContext;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string userName, string password, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            var isAuthenticated = await _signInManager.PasswordSignInAsync(userName, password, true, false);
+            if (isAuthenticated.Succeeded)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim("user", userName),
+                    new Claim("role", "Member")
+                };
+
+                await HttpContext.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity(claims, "Cookies", "user", "role")));
+
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return Redirect("/");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        public async Task<IdentityResult> Register(NewUserRequest newUser)
+        {
+            var user = new Models.User.User()
+            {
+                Email = newUser.EmailAddress,
+                UserName = newUser.UserName,
+                PhoneNumber = newUser.PhoneNumber,
+            };
+
+            var hasher = new PasswordHasher<User>();
+            var hashedPassword = hasher.HashPassword(user, newUser.Password);
+            user.PasswordHash = hashedPassword;
+
+            return await _userManager.CreateAsync(user);
+        }
+
+        public IActionResult AccessDenied(string returnUrl = null)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
+    }
+}
---
+++ AutoPartsApp/Models/User/Request/NewUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoPartsApp.Models.User.Request
+{
+    public class NewUserRequest
+    {
+        public string EmailAddress { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
